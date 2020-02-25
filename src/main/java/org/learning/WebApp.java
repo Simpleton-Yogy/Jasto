4c6d6af1d29c41ce6e1d4a988198b3d356028506
Simpleton-Yogy <jiri.s.15243@gmail.com>
2020-02-25 22:44:44 +0100

    Added all existing files

---
+++ src/main/java/org/learning/WebApp.java
@@ -0,0 +1,16 @@
+package org.learning;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApp {
+
+    public static void main(String[] args){
+
+        SpringApplication.run(WebApp.class, args);
+
+    }
+
+}
